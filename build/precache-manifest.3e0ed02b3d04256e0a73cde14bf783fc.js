207ef603b2dcc563f34636d4af6a06f3f4ffafd2
Coleman Hedge <chedge@indeed.com>
2024-01-04 11:33:06 -0800

    adding build file

---
+++ build/precache-manifest.3e0ed02b3d04256e0a73cde14bf783fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45cfa5e6a569098751bfb8414341c80e",
+    "url": "/recipe-crud/index.html"
+  },
+  {
+    "revision": "fb1fcbe1bf63c4e191e6",
+    "url": "/recipe-crud/static/css/main.7b8d804e.chunk.css"
+  },
+  {
+    "revision": "dbf19fd96ea7ed36de7a",
+    "url": "/recipe-crud/static/js/2.e1e0ccdf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/recipe-crud/static/js/2.e1e0ccdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb1fcbe1bf63c4e191e6",
+    "url": "/recipe-crud/static/js/main.b6986294.chunk.js"
+  },
+  {
+    "revision": "920aab6b1ceb85671c81",
+    "url": "/recipe-crud/static/js/runtime-main.cd5bd6cd.js"
+  }
+]);
